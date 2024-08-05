56b8b02bbc1cbba2d63a4e80219a565456886aa4
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-08-05 16:05:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+API_KEY = 'aSPkiWrlnLzest6sUOmxz8tIY06v7MzAmdkUxRFupomyj3400MABC0k0wr5erTCE'
+API_SECRET = 'JZGHUi2b8B1pQBD2zf1kKnJr7h18P8vghVN3JrVvgP5WD9WnvylW3HETXGERMJo3'
+THREE_COMMAS_WEBHOOK_URL = 'https://api.3commas.io/signal_bots/webhooks'
+SELECTED_SYMBOLS = ['BTC/USDT', 'ETH/USDT']  # Add your desired symbols
